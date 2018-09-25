ee4bea4ea39c9fa85a47526b938ef4e9b1db642b
ionawright <ionawright@live.com>
2018-09-25 18:28:24 +0100

    save_method_added

--- console.rb
+++ console.rb
@@ -1,15 +1,21 @@
-require_relative("./models/space_cowboys.rb")
+require_relative("./models/bounty_finder.rb")
+require("pry")
 
   criminal1 = Bounty.new({
-    "name" => "Gerrard Butler"
-    "favourite_weapon" => "Gun"
-    "danger_level" => "High"
-    "bounty_value" => 80,000
-    })
+    "name" => "Gerrard Butler",
+    "favourite_weapon" => "Gun",
+    "danger_level" => "High",
+    "bounty_value" => 80000
+  })
 
   criminal2 = Bounty.new({
-    "name" => "Jennifer Aniston"
-    "favourite_weapon" => "Rope"
-    "danger_level" => "Medium"
-    "bounty_value" => 40,000
-    })
+    "name" => "Jennifer Aniston",
+    "favourite_weapon" => "Rope",
+    "danger_level" => "Medium",
+    "bounty_value" => 40000
+  })
+
+criminal1.save()
+
+binding.pry
+nil
--- models/bounty_finder.rb
+++ models/bounty_finder.rb
@@ -12,5 +12,68 @@ class Bounty
     @bounty_value = options ["bounty_value"].to_i()
   end
 
+  def save()
+    db = PG.connect ({
+    dbname: "bounty_hunter",
+    host: "localhost" })
+
+      sql = "
+        INSERT INTO bounty_finder(
+          name,
+          favourite_weapon,
+          danger_level,
+          bounty_value
+        )
+        VALUES ($1, $2, $3, $4)
+        RETURNING id;
+      "
+      db.prepare("save", sql)
+      db.exec_prepared("save", [
+          @name,
+          @favourite_weapon,
+          @danger_level,
+          @bounty_value
+        ])
+      db.close()
+  end
+
+  def self.delete_all()
+    db = PG.connect ({
+    dbname: "bounty_hunter",
+    host: "localhost" })
+
+  sql = "SELECT * FROM bounty_finder;"
+  db.prepare("all", sql)
+
+  
+  end
+
+  def update()
+    db = PG.connect({
+    dbname: "bounty_hunter",
+    host: "localhost" })
+
+    sql = "
+    UPDATE bounty_finder
+    SET (
+      name,
+      favourite_weapon,
+      danger_level,
+      bounty_value
+    ) = ($1, $2, $3, $4) WHERE id = $5;
+    "
+
+    values = [
+      @name,
+      @favourite_weapon,
+      @danger_level,
+      @bounty_value
+    ]
+
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close()
+
+  end
 
 end
