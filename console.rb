84189327b44ac885ebde09e10898094439adf466
ionawright <ionawright@live.com>
2018-09-25 16:24:10 +0100

    first commmit

---
+++ console.rb
@@ -0,0 +1,15 @@
+require_relative("./models/space_cowboys.rb")
+
+  Cowboy1 = Bounty.new({
+    "name" => "Gerrard Butler"
+    "favourite_weapon" => "Gun"
+    "danger_level" => "High"
+    "bounty_value" => 80,000
+    })
+
+  Cowboy2 = Bounty.new({
+    "name" => "Jennifer Aniston"
+    "favourite_weapon" => "Rope"
+    "danger_level" => "Medium"
+    "bounty_value" => 40,000
+    })
---
+++ models/space_cowboys.rb
@@ -0,0 +1,15 @@
+class Bounty
+
+  attr_accessor :name, :favourite_weapon, :danger_level, :bounty_value
+  attr_reader :id
+
+  def initialize(options)
+    @id = options["id"].to_i()
+    @name = options["name"]
+    @favourite_weapon = options["favourite_weapon"]
+    @danger_level = options["danger_level"]
+    @bounty_value = options ["bounty_value"].to_i()
+  end
+
+
+end
